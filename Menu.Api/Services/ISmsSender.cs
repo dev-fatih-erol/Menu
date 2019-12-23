c25c498469dabcae3ae1ccb0ef84c548a38e466f
dev.fatih.erol <dev.fatih.erol@gmail.com>
2019-12-23 23:02:40 +0300

    .

---
+++ Menu.Api/Services/ISmsSender.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Menu.Api.Services
+{
+    public interface ISmsSender
+    {
+        Task<string> Send(string phoneNumber, string message);
+    }
+}
---
+++ Menu.Api/Services/SmsSender.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.WebUtilities;
+
+namespace Menu.Api.Services
+{
+    public class SmsSender : ISmsSender
+    {
+        public async Task<string> Send(string phoneNumber, string message)
+        {
+            var queryStringParameters = new Dictionary<string, string>()
+            {
+               {"usercode", "5394257609"},
+               {"password", "51A8C2"},
+               {"gsmno", phoneNumber },
+               {"message", message},
+               {"msgheader", "Motto Mobil"},
+               {"dil", "TR"}
+            };
+
+            var url = QueryHelpers.AddQueryString("https://api.netgsm.com.tr/sms/send/get", queryStringParameters);
+
+            using var httpClient = new HttpClient();
+
+            var response = await httpClient.GetAsync(url);
+
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
