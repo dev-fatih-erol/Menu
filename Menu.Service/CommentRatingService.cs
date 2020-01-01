be3911b14cee1acb5471fb3be89031dbb18bcebf
dev.fatih.erol <dev.fatih.erol@gmail.com>
2020-01-02 00:26:44 +0300

    .

---
+++ Menu.Service/CommentRatingService.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Menu.Core.Models;
+using Menu.Data;
+
+namespace Menu.Service
+{
+    public class CommentRatingService : ICommentRatingService
+    {
+        private readonly MenuContext _context;
+
+        public CommentRatingService(MenuContext context)
+        {
+            _context = context;
+        }
+
+        public List<CommentRating> GetByVenueId(int venueId)
+        {
+            return _context.CommentRatings
+                           .Where(c => c.VenueId == venueId)
+                           .Select(c => new CommentRating
+                           {
+                               Id = c.Id,
+                               Text = c.Text,
+                               Speed = c.Speed,
+                               Waiter = c.Waiter,
+                               Flavor = c.Flavor,
+                               User = c.User
+                           }).ToList();
+        }
+
+        public void Create(CommentRating commentRating)
+        {
+            _context.CommentRatings.Add(commentRating);
+        }
+
+        public void SaveChanges()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ Menu.Service/ICommentRatingService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Menu.Core.Models;
+
+namespace Menu.Service
+{
+    public interface ICommentRatingService
+    {
+        List<CommentRating> GetByVenueId(int venueId);
+
+        void Create(CommentRating commentRating);
+
+        void SaveChanges();
+    }
+}
