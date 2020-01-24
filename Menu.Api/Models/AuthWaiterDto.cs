1db2581d7156f55585b041614eeb6309f8c3142b
dev.fatih.erol <dev.fatih.erol@gmail.com>
2020-01-24 15:50:43 +0300

    .

--- Menu.Api/Models/AuthWaiterDto.cs
+++ Menu.Api/Models/AuthWaiterDto.cs
@@ -1,9 +1,16 @@
-﻿namespace Menu.Api.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Menu.Api.Models
 {
     public class AuthWaiterDto
     {
+        [Required(ErrorMessage = "Lütfen şifrenizi girin")]
+        [RegularExpression("^(?=.{8,20}$)(?![_.])(?!.*[_.]{2})[a-zA-Z0-9._]+(?<![_.])$", ErrorMessage = "Lütfen geçerli bir kullanıcı adı girin")]
         public string Username { get; set; }
 
+        [Required(ErrorMessage = "Lütfen şifrenizi girin")]
+        [MinLength(6, ErrorMessage = "Şifre en az 6 karakter uzunluğunda olmalıdır")]
+        [RegularExpression(@"^[a-zA-Z0-9çıüğöşİĞÜÖŞÇ]+$", ErrorMessage = "Lütfen geçerli bir şifre girin")]
         public string Password { get; set; }
     }
 }
