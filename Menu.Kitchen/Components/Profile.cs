55c4f1893b74a0c6359ab0976a2f3e2260dfb1ef
dev.fatih.erol@gmail.com <dev.fatih.erol>
2020-01-29 01:38:46 +0300

    .

---
+++ Menu.Kitchen/Components/Profile.cs
@@ -0,0 +1,30 @@
+﻿using Menu.Kitchen.Extensions;
+using Menu.Kitchen.Models.ComponentViewModels;
+using Menu.Service;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Menu.Kitchen.Components
+{
+    public class Profile : ViewComponent
+    {
+        private readonly IKitchenService _kitchenService;
+
+        public Profile(IKitchenService kitchenService)
+        {
+            _kitchenService = kitchenService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var kitchen = _kitchenService.GetById(User.Identity.GetId());
+
+            var model = new ProfileViewModel
+            {
+                KitchenName = kitchen.Name,
+                VenueName = kitchen.Venue.Name
+            };
+
+            return View(model);
+        }
+    }
+}
