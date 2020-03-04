84832f5ce0145676140ffc014c1ce25ac3f6d4cb
dev-fatih-erol <dev.fatih.erol@gmail.com>
2020-03-04 04:16:19 +0300

    .

---
+++ Menu.Business/Models/HomeViewModels/IndexViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Menu.Business.Models.HomeViewModels
+{
+    public class IndexViewModel
+    {
+        [Required(ErrorMessage = "Lütfen kullanıcı adınızı girin")]
+        [StringLength(60, MinimumLength = 6, ErrorMessage = "Lütfen geçerli kullanıcı adı girin")]
+        [RegularExpression(@"^[A-Za-z0-9-_.]+$", ErrorMessage = "Lütfen geçerli kullanıcı adı girin")]
+        public string Username { get; set; }
+
+        [Required(ErrorMessage = "Lütfen şifrenizi girin")]
+        [StringLength(16, MinimumLength = 6, ErrorMessage = "Lütfen geçerli şifre girin")]
+        [RegularExpression(@"^[A-Za-z0-9-_.]+$", ErrorMessage = "Lütfen geçerli şifre girin")]
+        public string Password { get; set; }
+    }
+}
